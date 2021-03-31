f3d03c4ba4a6ee5e40c7a496024670124ade5470
BrakeLJ <lukejbrake@gmail.com>
2021-03-30 20:22:56 -0400

    started logic

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = () => {
+  const song = document.querySelector('.song');
+  const play = document.querySelector('.play');
+  const outline = document.querySelector('.moving-outline circle');
+  const video = document.querySelector('.video-container video');
+
+  //Sounds
+  const sounds = document.querySelectorAll('.sound-picker');
+  //Time display
+  const timeDisplay = document.querySelector('.time-display');
+};
+
+app();
